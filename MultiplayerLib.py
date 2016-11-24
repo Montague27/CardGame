3ed24423416f47ca560cfc357094eba439eb80d2
ChuShu <aaas1253XT@gmail.com>
2016-11-24 18:14:27 +0800

    Syntax to Python3

--- MultiplayerLib.py
+++ MultiplayerLib.py
@@ -9,80 +9,96 @@ import socket
 
 #I think there should be a coin fliiping or some sort of method to determine
 #who goes first.
-def coinflip:
-	pass
+def coinflip():
+    pass
 
 #Make the JSON object for request
 def serialize(request):
-	if(type(request) is not dict):
-		print "Please pass a dict as an action object."
-	return (json.dumps(request));
+    if type(request) is not dict:
+        print('Please pass a dict as an action object.')
+        return json.dumps(request)
 
 #Deserialize message received
 def deserialize(message):
-	if(type(message) is not dict):
-		print "Error: the message cannot be parsed"
-	return (json.loads(message))
+    if type(message) is not dict:
+        print('Error: the message cannot be parsed')
+        return json.loads(message)
 
 #A general socket object
 class generalSocket(object):
-	#constructor
-	def __init__(self, serverIpAddr="127.0.0.1", port=8964, timeout=None):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self._timeout = timeout
-		self._conn = None
-        	self._address = address
-	        self._port = port
-	#Printing Error messages: should be changed
-	def _printDebug(message):
-		print message
-	#get private read-only properties
-	def getAddress(self):
-		return self._address
-	def getPort(self):
-		return self._port
-	def getTimeout(self):
-		return self._timeout
-	def isConnected(self):
-		return (self._conn == None)
-	def sendData(self,data):
-		if self.isConnected():
-			self._conn.send(data);
-		else return "Error"
-	def recvData(self):
-		data = self._conn.recv(8192)
-	def closeSocket(self):
-		self.socket.shutdown(SHUT_RDWR)
-		self.socket.close()
+    #constructor
+    def __init__(self, serverIpAddr='127.0.0.1', port=8964, timeout=None):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._timeout = timeout
+        self._conn = None
+        self._address = address
+        self._port = port
+        
+    #Printing Error messages: should be changed
+    def _printDebug(message):
+        print(message)
+        
+    #get private read-only properties
+    def getAddress(self):
+        return self._address
+
+    def getPort(self):
+        return self._port
+
+    def getTimeout(self):
+        return self._timeout
+
+    def isConnected(self):
+        return (self._conn == None)
+
+    def sendData(self,data):
+        if self.isConnected():
+            self._conn.send(data)
+        else:
+            return 'Error'
+    def recvData(self):
+        data = self._conn.recv(8192)
+        
+    def closeSocket(self):
+        self.socket.shutdown(SHUT_RDWR)
+        self.socket.close()
 
 class serverSocket(generalSocket):
-	def __init__(self, serverIpAddr, port=8964, timeout=None):
-		super(serverSocket,self).__init__(self.serverIpAddr, self.port, self.timeout)
-	def startServer:
-		self.socket.bind((self._address,self._port))
-		self.socket.listen(1)
-		self._conn, client = self.socket.accept()
-		self._printDebug("Connection received from client "+client)
-        	self._conn.settimeout(self.timeout)
-	#closes the connection
-	def closeServer:
-		self._conn.shutdown(SHUT_RDWR)
-		self._conn.close()
-		self.closeSocket();
-	#Tell the remote side that the connection is closing
-	def closeRemote:
-		self._conn.send(serialize({"action": "close"});
+    def __init__(self, serverIpAddr, port=8964, timeout=None):
+        super(serverSocket,self).__init__(self.serverIpAddr, self.port, self.timeout)
+    def startServer():
+        self.socket.bind((self._address, self._port))
+        self.socket.listen(1)
+        self._conn, client = self.socket.accept()
+        self._printDebug('Connection received from client ' + client)
+        self._conn.settimeout(self.timeout)
+        
+    #closes the connection
+    def closeServer():
+        self._conn.shutdown(SHUT_RDWR)
+        self._conn.close()
+        self.closeSocket()
+        
+    #Tell the remote side that the connection is closing
+    def closeRemote():
+        self._conn.send(serialize({"action": "close"}))
 
+          
 class clientSocket(generalSocket):
-	def __init__(self,serverIpAddr,port=8964,timeout=None):
-		super(clientSocket,self).__init__(self.serverIpAddr, self.port, self.timeout)
-	def closeRemote:
-		self.socket.send(serialize({"action": "close"});
-	def connect(self,serverIpAddr):
-		self.socket.connect((self._address,self.port))
-	def closeClient:
-		self.closeSocket();
+    def __init__(self, serverIpAddr, port=8964, timeout=None):
+        super(clientSocket,self).__init__(self.serverIpAddr, self.port, self.timeout)
+        
+        def closeRemote():
+            self.socket.send(serialize({"action": "close"}))
+            
+        def connect(self ,serverIpAddr):
+            self.socket.connect((self._address, self.port))
+            
+        def closeClient():
+            self.closeSocket()
 
+my_socket = generalSocket()
+client = clientSocket(my_socket)
 """
 README:
 To actually implement a multiplayer game, other than starting the sockets and connections, you will have to:
